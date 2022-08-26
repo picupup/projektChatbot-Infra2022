7c04c259820019c8fa78b936eaf0ae07bb24bbd0
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-08-26 10:10:01 +0200

    Update von 2022-08-26___10:10:01

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+dst=/var/www/html/infra-2022-e/
+cp -r www/* $dst
+currentdate=$(date)
+echo "$currentdate" > $dst/happy.txt
+happy=$(curl -s https://informatik.hs-bremerhaven.de/infra-2022-e/happy.txt)
+echo "$happy"
+echo "$currentdate"
+if ["$happy" = "$currentdate"]; then
+  echo happy
+fi
+curl -s https://informatik.hs-bremerhaven.de/infra-2022-e/*|html2text -utf8
