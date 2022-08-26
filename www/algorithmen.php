2907cd500ff0c3f70397ffec1979455a18907e7a
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-08-26 12:30:02 +0200

    Update von 2022-08-26___12:30:02

--- www/algorithmen.php
+++ www/algorithmen.php
@@ -1,42 +1,53 @@
 <?php
 //q = questions
-function search (q){
-  include ("db_connection.php")
+function search ($q){
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
+  include ("db_connection.php");
   $conn=return_db_connection();
   $sql="select bot_qa.q, bot_qa.a FROM bot_qa";
   $result = mysqli_query($conn, $sql);
 
-    $string = "";
-    while($row = mysqli_fetch_assoc($result)) {
+  $string = "";
+    
+  $bigger_count = 0;
+  $current_count = 0;
+  $current_question;
+  $target_answer;
+  $current_answer;
+
+   while($row = mysqli_fetch_assoc($result)) {
         $cond=0;
-        $last_count = 0;
-        $current_count = 0;
-        $target_answer;
-        $current_question;
-        $current_answer;
         foreach($row as $item){
-          ++$cond
-            if ($cond % 2 = 0){ //Modulo, weil die daten paarweise trennen werden müssen
+            if ($cond % 2 == 0){ //Modulo, weil die daten paarweise trennen werden müssen
                 $current_question = $item;
             }
             else{
                 $current_answer = $item;
             }
+            
+          ++$cond;
         }
 
-        $cond=true;
-        $string .= "<option value=\" $eins \"> $zwei </option>";
-        //echo "<option value=\" $eins \"> $zwei </option>";
-        //$data[] = $row;
+    $words = explode(" ", $current_question); // trenne die wörter im Array
+    
+    $current_count = 0;
+    foreach ($words as $word) {
+      if (str_contains($q, $word)){
+        ++$current_count;
+        echo $word;
+      }
+    }
+    if ($current_count > $bigger_count){
+      $bigger_count = $current_count;
+      $target_answer = $current_answer;
+      }
     };
-    return "$string";
+    return "$target_answer";
 
 }
-
-
-
-
-
 
 
 ?>
--- www/db_connection.php
+++ www/db_connection.php
@@ -6,14 +6,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 function return_db_connection(){
-    $groupUser = "swe2_2022team17";
+    $groupUser = "infra-2022-e";
     $benutzer = getenv('USER');
     if($benutzer != $groupUser){
         $pfad = "/var/www/html/$benutzer/private/dbconnection.inc.php";
     }else{
-        $pfad = "/var/www/html/swe2_2022team17/private/dbconnection.inc.php";
+        $pfad = "/var/www/html/$benutzer/private/dbconnection.inc.php";
     }
-    $pfad = "/var/www/html/swe2_2022team17/private/dbconnection.inc.php";
+    $pfad = "/var/www/html/$benutzer/private/dbconnection.inc.php";
     
     include($pfad);
     $conn = new mysqli($host, $user, $password, $database);
