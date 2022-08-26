482bb15d0b12d14caecdc5f52c2aaaca67eb3ab6
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-08-26 23:15:01 +0200

    Update von 2022-08-26___23:15:01

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-dst=/var/www/html/infra-2022-e/
-cp -r www/* $dst
-currentdate=$(date)
-echo "$currentdate" > $dst/happy.txt
-happy=$(curl -s https://informatik.hs-bremerhaven.de/infra-2022-e/happy.txt)
-echo "$happy"
-echo "$currentdate"
-if ["$happy" = "$currentdate"]; then
-  echo happy
-fi
-curl -s https://informatik.hs-bremerhaven.de/infra-2022-e/*|html2text -utf8
+# dst=/var/www/html/infra-2022-e/
+# cp -r www/* $dst
+# currentdate=$(date)
+# echo "$currentdate" > $dst/happy.txt
+# happy=$(curl -s https://informatik.hs-bremerhaven.de/infra-2022-e/happy.txt)
+# echo "$happy"
+# echo "$currentdate"
+# if ["$happy" = "$currentdate"]; then
+#   echo happy
+# fi
+# curl -s https://informatik.hs-bremerhaven.de/infra-2022-e/*|html2text -utf8
 
-dst2=/var/www/html/docker-infra-2022-e-web/
-scp www/* mydocker:$dst2
+
+
+dst2=/var/www/html/docker-$USER-web/
+ssh mydocker "mkdir -p $dst2"
+scp -r www/* mydocker:$dst2
 
