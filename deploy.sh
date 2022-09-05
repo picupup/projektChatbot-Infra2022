732dc52d6477c6e62ae7cc9e5ea40ce7efc85bee
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-05 15:55:09 +0200

    Update von 2022-09-05___15:55:09

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-#sudo -i -u infra-2022-e ssh mydocker "mkdir -p /var/www/html/docker-infra-2022-e-web/robbi/"
-#scp www/* | (sudo -i -u infra-2022-e scp -rq mydocker:$dst)
-#rsync www/* infra-2022-e@hopper.hs-bremerhaven.de:$dst 
+echo "###########################################################################"
+echo "Link: https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/"
+echo "Deine Änderungen werden erst Sichtbar, nachdem 'git push' ausgeführt wurde."
+echo "###########################################################################"
 
-#sudo -i -u infra-2022-e cd /home/infra-2022-e/repos/infra-2022-e/ git pull
 sudo -i -u infra-2022-e git -C /home/infra-2022-e/repos/infra-2022-e/ pull
 sudo -i -u infra-2022-e sh /home/infra-2022-e/repos/infra-2022-e/ldeploy.sh
