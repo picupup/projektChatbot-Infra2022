1454f9187e9f52cbddc0020b3ab48d7be281be94
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-06 14:40:03 +0200

    Update von 2022-09-06___14:40:03

--- lastTest/curlCommand.sh
+++ lastTest/curlCommand.sh
@@ -4,8 +4,9 @@
 #2022-09-06_12:47:18
 #\Thema:This script is a one time curl of the website
 #
+
 input=${1:-"how are you"}
 input=$(echo "$input" |sed -E "s/ /%20/g")
 #echo "$input"
 
-curl "https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/call_algorithm.sh?question=$input" 
+curl -v "https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/call_test.sh?question=\'$input\'" | html2text
