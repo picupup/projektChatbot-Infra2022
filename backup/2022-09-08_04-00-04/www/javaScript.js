fc9484a9506e35587ba784dc8ba4d0a51df0d198
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-08 04:05:07 +0200

    Update von 2022-09-08___04:05:07

---
+++ backup/2022-09-08_04-00-04/www/javaScript.js
@@ -0,0 +1,30 @@
+//var form = document.forms['robbi'].element['question'].value
+
+
+console.log("aus der javaScript.js");
+
+
+function senden(){
+
+	// console.log("aus der submit-Funktion");
+
+	var form = document.forms['robbi']; //DOM (Document Object Model)
+	var form_question = form['question'].value;
+	console.log(form_question);
+	var xhr = new XMLHttpRequest() //Here begins Ajax
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+			document.getElementById("answer").innerHTML = (xhr.responseText)
+		}
+	}
+	xhr.open("GET", 'call_algorithm.php' + "?question='"+form_question+"'", true);
+	xhr.send();
+
+	form['question'].value = "";
+}
+
+
+// var message = form["name"].value
+
+
+
