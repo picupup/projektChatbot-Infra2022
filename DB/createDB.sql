be42c17bbb5a8ba469df799921adba8822946afa
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-09 18:30:03 +0200

    Update von 2022-09-09___18:30:03

--- DB/createDB.sql
+++ DB/createDB.sql
@@ -1,5 +1,5 @@
 DROP table if exists bot_qa;
-
+DROP table if exists bot_login;
 
 
 create table bot_qa(
@@ -10,8 +10,17 @@ create table bot_qa(
 );
 
 
+create table bot_login(
+  id int AUTO_INCREMENT,
+  email TEXT,
+  password TEXT,
+  hint TEXT,
+  PRIMARY KEY (id)
+);
 
 insert into bot_qa (q, a) values ('How are you','not so well');
 insert into bot_qa (q, a) values ('was up','not much');
 insert into bot_qa (q, a) values ('How are you doing','doing well');
 insert into bot_qa (q, a) values ('How are you today','today im so happy');
+insert into bot_login (email, password, hint) values ('123@gmail.com','123', 'OneTwoThree');
+
