278968b50c8a2ede7749db2a70d167e6c8ada869
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-11 21:40:04 +0200

    Update von 2022-09-11___21:40:04

--- lastTest/curlMitsed.sh
+++ lastTest/curlMitsed.sh
@@ -8,9 +8,25 @@
 input=${1:-"how are you"}
 input=$(echo "$input" |sed -E "s/ /%20/g")
 #echo "$input"
-echo "$(date '+%Y-%m-%d_%H:%M:%S')"
+./startstate.sh
 
-seq 10000 | parallel --max-args 0 --jobs 100 "curl -X 'GET' -s https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/call_test.php?question='$input'"
+dateBegin="$(date '+%Y-%m-%d')"
+hourBegin="$(date '+%H:%M:%S')"
+
+seq 10000 | parallel --max-args 0 --jobs 1020 "curl -X 'GET' -s https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/call_test.php?question='$input'" >/dev/null
 
 
-echo "$(date '+%Y-%m-%d_%H:%M:%S')"
+#echo "$(date '+%Y-%m-%d_%H:%M:%S')"
+
+dateEnd="$(date '+%Y-%m-%d')"
+hourEnd="$(date '+%H:%M:%S')"
+
+
+f1=~/tmp/dockerstatswatch/data.txt
+
+echo -e "Data will be shwon know \n $dateBegin $hourBegin" 
+
+
+cat $f1 | sed -n "/^$dateBegin $hourBegin/,/^$dateEnd $hourEnd/p" | cut -d " " -f 9 |tr -d "%"
+
+echo -e "\n$dateEnd $hourEnd"
---
+++ lastTest/startstate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#<<startstate.sh>>
+#team e infra
+#2022-09-11_17:46:50
+#\Thema: save docker statistcs in a file
+# Please excecute this once
+
+mkdir -p ~/tmp
+mkdir -p ~/tmp/dockerstatswatch
+touch ~/tmp/dockerstatswatch/data.txt
+touch ~/tmp/dockerstatswatch/err.log
+
+f1=~/tmp/dockerstatswatch/data.txt
+f2=~/tmp/dockerstatswatch/err.log
+
+echo -e "" > $f1
+
+hbv_dockerstatswatch > $f1 2> $f2 &
+
+echo $!
