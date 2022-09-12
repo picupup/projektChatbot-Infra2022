c9fa0534276ba66e69893664d3492b70d5260a27
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-13 00:40:05 +0200

    Update von 2022-09-13___00:40:05

--- lastTest/curlMitsed.sh
+++ lastTest/curlMitsed.sh
@@ -8,12 +8,12 @@
 input=${1:-"how are you"}
 input=$(echo "$input" |sed -E "s/ /%20/g")
 #echo "$input"
-./startstate.sh
-
+#./startstate.sh
+sleep 0.7
 dateBegin="$(date '+%Y-%m-%d')"
 hourBegin="$(date '+%H:%M:%S')"
 
-seq 10000 | parallel --max-args 0 --jobs 1020 "curl -X 'GET' -s https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/call_test.php?question='$input'" >/dev/null
+seq 1000 | parallel --max-args 0 --jobs 100 "curl -X 'GET' -s https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/call_test.php?question='$input'" > /dev/null
 
 
 #echo "$(date '+%Y-%m-%d_%H:%M:%S')"
@@ -21,16 +21,17 @@ seq 10000 | parallel --max-args 0 --jobs 1020 "curl -X 'GET' -s https://informat
 dateEnd="$(date '+%Y-%m-%d')"
 hourEnd="$(date '+%H:%M:%S')"
 
+sleep 0.7
 
 f1=~/tmp/dockerstatswatch/data.txt
 
-echo -e "Data will be shwon now \n $dateBegin $hourBegin" 
+echo -e "Data will be shown now \n $dateBegin $hourBegin" 
 
-
-cat $f1 | sed -n "/^$dateBegin $hourBegin/,/^$dateEnd $hourEnd/p" | cut -d "%" -f 1 | rev | cut -d " " -f 1 | rev
+cat $f1 | sed -n "/^$dateBegin $hourBegin/,/^$dateEnd $hourEnd/p" | cut -d "%" -f 1 | rev | cut -d " " -f 1 | rev > img.txt
+#cat $f1 | sed -n "/$dateBegin $hourBegin/,/$dateEnd $hourEnd/p"
 
 mkdir -p ~/tmp
 f2=~/tmp/last.gp
 touch $f2
   #Gnuplot erzeugen
-echo -e "\n $dateEnd $hourEnd" 
+echo -e "\n $dateEnd $hourEnd"
