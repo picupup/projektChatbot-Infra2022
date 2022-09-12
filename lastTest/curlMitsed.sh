08712cdecb9f90e139e2bc9dd3739997ffc9bb20
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-13 01:20:05 +0200

    Update von 2022-09-13___01:20:04

--- lastTest/curlMitsed.sh
+++ lastTest/curlMitsed.sh
@@ -3,7 +3,7 @@
 #team infra 2022
 #2022-09-06_12:47:18
 #\Thema:This script is a one time curl of the website
-#
+# Wichtig da dockerstatwatch einmaig für immer läuft sollt ./startstate.sh einmalig durchgeführt werden.
 
 input=${1:-"how are you"}
 input=$(echo "$input" |sed -E "s/ /%20/g")
@@ -12,6 +12,7 @@ input=$(echo "$input" |sed -E "s/ /%20/g")
 sleep 0.7
 dateBegin="$(date '+%Y-%m-%d')"
 hourBegin="$(date '+%H:%M:%S')"
+echo -e "Starting at \n $dateBegin $hourBegin" 
 
 seq 1000 | parallel --max-args 0 --jobs 100 "curl -X 'GET' -s https://informatik.hs-bremerhaven.de/docker-infra-2022-e-web/robbi/call_test.php?question='$input'" > /dev/null
 
@@ -24,14 +25,13 @@ hourEnd="$(date '+%H:%M:%S')"
 sleep 0.7
 
 f1=~/tmp/dockerstatswatch/data.txt
+imgtxt=~/tmp/dockerstatswatch/img.txt
+touch ~/tmp/dockerstatswatch/img.txt
 
-echo -e "Data will be shown now \n $dateBegin $hourBegin" 
-
-cat $f1 | sed -n "/^$dateBegin $hourBegin/,/^$dateEnd $hourEnd/p" | cut -d "%" -f 1 | rev | cut -d " " -f 1 | rev > img.txt
+cat $f1 | sed -n "/^$dateBegin $hourBegin/,/^$dateEnd $hourEnd/p" | cut -d "%" -f 1 | rev | cut -d " " -f 1 | rev > $imgtxt
 #cat $f1 | sed -n "/$dateBegin $hourBegin/,/$dateEnd $hourEnd/p"
+./timeCreator.sh
+./createPlotPng.sh
+echo -e "\nEnding at $dateEnd $hourEnd"
 
-mkdir -p ~/tmp
-f2=~/tmp/last.gp
-touch $f2
-  #Gnuplot erzeugen
-echo -e "\n $dateEnd $hourEnd"
+echo -e "check out the results under \n https://informatik.hs-bremerhaven.de/$USER/last.png"
