2e3474fb91be807d0f49160bf30df90ffdc5975a
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-18 13:00:07 +0200

    Update von 2022-09-18___13:00:07

--- ldeploy.sh
+++ ldeploy.sh
@@ -1,14 +1,17 @@
 #/bin/bash
+## which user is executing?
 if [ $USER = "infra-2022-e" ]
 	then
 		repo=/home/$USER/repos/$USER
 	else
 		repo=$PWD
 	fi
-
+## preparations
 dst=/var/www/html/docker-$USER-web/robbi
 ssh mydocker "mkdir -p $dst"
+## mirroring
 rsync -av $repo/www/ mydocker:$dst --delete --progress
+## Happybit
 currentdate=$(date)
 echo "$currentdate" > happy.txt
 scp -q happy.txt mydocker:$dst/
@@ -16,10 +19,15 @@ rm happy.txt
 happy=$(curl -s https://informatik.hs-bremerhaven.de/docker-$USER-web/robbi/happy.txt)
 echo "$happy"
 echo "$currentdate"
-if [ "$happy" = "$currentdate" ]; then
+if [ "$happy" = "$currentdate" ]; 
+then
    echo happy
- fi
-curl -s https://informatik.hs-bremerhaven.de/docker-$USER-web/robbi/chat.php |html2text -utf8
+else
+   echo unhappy!
+   exit
+fi
+ ## Output what the browser is showing.
+curl -s https://informatik.hs-bremerhaven.de/docker-$USER-web/robbi/chat.php | html2text -utf8
 
 
 
