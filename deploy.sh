78a36bf9e1a42d3723868f7ccb83f1615e3ede4b
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-19 17:15:09 +0200

    Update von 2022-09-19___17:15:09

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ echo "Deine Änderungen werden erst Sichtbar, nachdem 'git push' ausgeführt wur
 echo "###########################################################################"
 
 sudo -i -u infra-2022-e git -C /home/infra-2022-e/repos/infra-2022-e/ pull
-sudo -i -u infra-2022-e sh /home/infra-2022-e/repos/infra-2022-e/ldeploy.sh
+sudo -i -u infra-2022-e bash /home/infra-2022-e/repos/infra-2022-e/ldeploy.sh
+sudo -i -u infra-2022-e bash /home/infra-2022-e/repos/infra-2022-e/erstelle_DB_zugang.sh
