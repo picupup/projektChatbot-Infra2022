b7e8b0a8b2eeedd727532c71b7d549607d55b286
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-20 20:05:10 +0200

    Update von 2022-09-20___20:05:09

--- lastTest/test.php
+++ lastTest/test.php
@@ -10,9 +10,8 @@ error_reporting(E_ALL);
 
 $user=get_current_user();
 $dir="/var/www/html/infra-2022-e/test_result";
-$dir2=__DIR__ . './test_result';
 $files = scandir($dir);
-$files2 = array_diff(scandir($dir), array('..', '.'));
+$files2 = array_diff(scandir($dir), array('..', '.')); // remove dots
 $ww="https://informatik.hs-bremerhaven.de/infra-2022-e/test_result";
 //print_r($files1);
 //
@@ -24,8 +23,8 @@ echo "<html>
 <body>";
 
 foreach ($files2 as $file) {
-  echo "<label>$file</label><br>\n";
-  echo "<img src=\"$ww/$file\" alt=\"Test with complexity of 1000 X $file\">\n";
+  echo "<label>Test with complexity of 1000 X $file</label><br>\n";
+  echo "<img src=\"$ww/$file\" alt=\"$ww/$file\">\n";
 }
 
 echo "</body>
