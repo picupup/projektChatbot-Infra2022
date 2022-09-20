ca8aebaafa4a32107966d64dc1aa0656369ebed1
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-20 18:35:09 +0200

    Update von 2022-09-20___18:35:09

--- lastTest/runTest.sh
+++ lastTest/runTest.sh
@@ -5,6 +5,7 @@
 #
 #\Thema: this script simpily runs the startTest.sh and gives it different Loop Numbers for the curl.sh script and after each test it saves the test result (png picture ) and carries on.
 testNr=${1:-8}
+mass=${2:-2000}
 loopNr=10
 counter=1
 out=/var/www/html/$USER
@@ -17,7 +18,7 @@ touch ~/tmp/test.log
 
 
 for i in $(seq 1 $testNr);do
-  loopNr=$(($i * 1000))
+  loopNr=$(($i * $mass))
   ./startTest.sh $loopNr > ~/tmp/test.log
   cp $out/last.png $result/$loopNr.png
   echo "https://informatik.hs-bremerhaven.de/$USER/test_result/$loopNr.png"
