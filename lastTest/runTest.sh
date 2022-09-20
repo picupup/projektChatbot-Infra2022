9858c16a75fcf5a944307a25ebea8e99b6229d7e
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-20 20:15:09 +0200

    Update von 2022-09-20___20:15:09

--- lastTest/runTest.sh
+++ lastTest/runTest.sh
@@ -11,16 +11,20 @@ counter=1
 out=/var/www/html/$USER
 result=$out/test_result
 mkdir -p $result
-rm -r $result/*
-out=/var/www/html/$USER
 mkdir -p ~/tmp
 touch ~/tmp/test.log
+testF=~/tmp/tmpTest
+mkdir -p $testF
+rm -r $testF/*
 
 
 for i in $(seq 1 $testNr);do
   loopNr=$(($i * $mass))
   echo "curl test Nr: 1000 x $loopNr"
   ./startTest.sh $loopNr > ~/tmp/test.log
-  cp $out/last.png $result/$loopNr.png
+  cp $out/last.png $testF/$loopNr.png
   echo "https://informatik.hs-bremerhaven.de/$USER/test_result/$loopNr.png"
 done
+  
+rm -r $result/*
+cp $testF/* $result/
