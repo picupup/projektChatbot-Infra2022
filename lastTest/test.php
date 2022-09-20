687268e2c43cef5065a9a5de0934562efba9efeb
Erfan Karimi <erfkarimi@smail.hs-bremerhaven.de>
2022-09-20 19:40:09 +0200

    Update von 2022-09-20___19:40:09

--- lastTest/test.php
+++ lastTest/test.php
@@ -4,12 +4,15 @@
 //2022-09-20_18:54:58
 //\Thema: This file shows all images in the test folder online
 //
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $user=get_current_user();
 $dir="/var/www/html/$user/test_result";
 $files = scandir($dir);
 $files2 = array_diff(scandir($dir), array('..', '.'));
-$ww="https://informatik.hs-bremerhaven.de/$user/test_result";
+$ww="https://informatik.hs-bremerhaven.de/infra-2022-e/test_result";
 //print_r($files1);
 //
 echo "<html>
